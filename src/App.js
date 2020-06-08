15fe86f886ee777f9c0103a400f126c9a69e6577
sandeepkamishetti <59001664+sandeepkamishetti@users.noreply.github.com>
2020-06-08 11:39:29 +0530

    Handling Patient's basic details in an array

--- src/App.js
+++ src/App.js
@@ -1,264 +1,268 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import AssessmentForm from './AssessmentForm.js';
 import Submit from './Submit';
 //import Main from './Main'
 //import Table from './Table';
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
     this.state = {
       renderView: 0,
-      patientName: "",
-      Date: "",
-      yhNum: "",
-      gender: "",
-      history: "None",
+      patientInfo: [
+        {
+          patientName: "",
+          Date: "",
+          yhNum: "",
+          gender: "",
+          history: "",
+          spurlingtest: "",
+          isRightLeftBoth:""
+        }],
       symptomsForPain:
-          [{
-              reason: "pain",
-              exists: "",
-              duration: 0
-          },
-          {
-              reason: "difficultyInLifting",
-              exists: "",
-              duration: 0
-          },
-          {
-              reason: "dislocation",
-              exists: "",
-              duration: 0
-          },
-          {
-              reason: "sleepLessNights",
-              exists: "",
-              duration: 0
-          },
-          {
-              reason: "swelling",
-              exists: "",
-              duration: 0
-          }],
+        [{
+          reason: "pain",
+          exists: "",
+          duration: 0
+        },
+        {
+          reason: "difficulty In Lifting",
+          exists: "",
+          duration: 0
+        },
+        {
+          reason: "dislocation",
+          exists: "",
+          duration: 0
+        },
+        {
+          reason: "sleepLess Nights",
+          exists: "",
+          duration: 0
+        },
+        {
+          reason: "swelling",
+          exists: "",
+          duration: 0
+        }],
       tenderness: [
-          {
-              type: "ANGLE OF ACROMION",
-              result: ""
-          },
-          {
-              type: "STERNO CLAVICULAR JOINT",
-              result: ""
-          },
-          {
-              type: "A-C JOINT",
-              result: ""
-          },
-          {
-              type: "BICIPITL GROOVE",
-              result: ""
-          },
-          {
-              type: "SPINE OF SCAPULA",
-              result: ""
-          },
-          {
-              type: "G-T OF THE HUMERUS",
-              result: ""
-          }
+        {
+          type: "ANGLE OF ACROMION",
+          result: ""
+        },
+        {
+          type: "STERNO CLAVICULAR JOINT",
+          result: ""
+        },
+        {
+          type: "A-C JOINT",
+          result: ""
+        },
+        {
+          type: "BICIPITL GROOVE",
+          result: ""
+        },
+        {
+          type: "SPINE OF SCAPULA",
+          result: ""
+        },
+        {
+          type: "G-T OF THE HUMERUS",
+          result: ""
+        }
 
       ],
       shoulderRangeOfMotionP: [
-          {
-              testName: "FORWARD FLEXION",
-              right: { active: 0, passive: 0 },
-              left: { active: 0, passive: 0 },
-          },
-          {
-              testName: "ABDUCTION",
-              right: { active: 0, passive: 0 },
-              left: { active: 0, passive: 0 },
-          }
+        {
+          testName: "FORWARD FLEXION",
+          right: { active: 0, passive: 0 },
+          left: { active: 0, passive: 0 },
+        },
+        {
+          testName: "ABDUCTION",
+          right: { active: 0, passive: 0 },
+          left: { active: 0, passive: 0 },
+        }
       ],
       shoulderRangeOfMotionN: [
-          {
-              testName: "EXTERNAL ROTATION",
-              right: { active: 0, passive: 0 },
-              left: { active: 0, passive: 0 },
-          },
-          {
-              testName: "INTERNAL ROTATION",
-              right: { active: 0, passive: 0 },
-              left: { active: 0, passive: 0 },
-          }
+        {
+          testName: "EXTERNAL ROTATION",
+          right: { active: 0, passive: 0 },
+          left: { active: 0, passive: 0 },
+        },
+        {
+          testName: "INTERNAL ROTATION",
+          right: { active: 0, passive: 0 },
+          left: { active: 0, passive: 0 },
+        }
       ],
       grom: [
-          {
-              testName: "GLOBAL RANGE OF MOTIONS",
-              result: { left: "", right: "" },
-          }
+        {
+          testName: "GLOBAL RANGE OF MOTIONS",
+          result: { left: "", right: "" },
+        }
       ],
       shoulderStrengthExam: [
-          {
-              examName: "FRENCH HORN SHOULDER TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "LIFT OFF SUBSCAPULARIS TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "EMPTY CAN TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "FULL CAN TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "DROP ARM SIGN",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "SPEEDS TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "YERGASON TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "ROTATOR CUFF TRIAD TEST",
-              result: { left: "", right: "" },
-          }
+        {
+          examName: "FRENCH HORN SHOULDER TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "LIFT OFF SUBSCAPULARIS TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "EMPTY CAN TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "FULL CAN TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "DROP ARM SIGN",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "SPEEDS TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "YERGASON TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "ROTATOR CUFF TRIAD TEST",
+          result: { left: "", right: "" },
+        }
       ],
       shoulderInstabilityExam: [
-          {
-              examName: "APPREHENSION",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "SHOULDER CROSSOVER MANNEOUVER",
-              result: { left: "", right: "" },
-          },
-          {
-              examName: "SULCUS SIGN",
-              result: { left: "", right: "" },
-          }
+        {
+          examName: "APPREHENSION",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "SHOULDER CROSSOVER MANNEOUVER",
+          result: { left: "", right: "" },
+        },
+        {
+          examName: "SULCUS SIGN",
+          result: { left: "", right: "" },
+        }
       ],
       shoulderImpingementSigns: [
-          {
-              testName: "NEER TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              testName: "HAWKINS TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              testName: "EMPTY CANS TEST",
-              result: { left: "", right: "" },
-          }
+        {
+          testName: "NEER TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          testName: "HAWKINS TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          testName: "EMPTY CANS TEST",
+          result: { left: "", right: "" },
+        }
 
       ],
       labralTearTest: [
 
-          {
-              testName: "SPEED TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              testName: "YERGASON TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              testName: "OBRIENS TEST",
-              result: { left: "", right: "" },
-          },
-          {
-              testName: "LABRAL CRANK TEST",
-              result: { left: "", right: "" },
-          }
+        {
+          testName: "SPEED TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          testName: "YERGASON TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          testName: "OBRIENS TEST",
+          result: { left: "", right: "" },
+        },
+        {
+          testName: "LABRAL CRANK TEST",
+          result: { left: "", right: "" },
+        }
       ],
       motorExam: [
 
-          {
-              whichNerve: "AXILLARY NERVE",
-              result: { left: "", right: "" },
-          },
-          {
-              whichNerve: "RADIAL NERVE",
-              result: { left: "", right: "" },
-          },
-          {
-              whichNerve: "ULNAR NERVE",
-              result: { left: "", right: "" },
-          },
-          {
-              whichNerve: "MEDIAN NERVE",
-              result: { left: "", right: "" },
-          }
+        {
+          whichNerve: "AXILLARY NERVE",
+          result: { left: "", right: "" },
+        },
+        {
+          whichNerve: "RADIAL NERVE",
+          result: { left: "", right: "" },
+        },
+        {
+          whichNerve: "ULNAR NERVE",
+          result: { left: "", right: "" },
+        },
+        {
+          whichNerve: "MEDIAN NERVE",
+          result: { left: "", right: "" },
+        }
       ],
       sensoryExam: [
 
-          {
-              whichNerve: "AXILLARY NERVE",
-              result: { left: "", right: "" },
-          },
-          {
-              whichNerve: "RADIAL NERVE",
-              result: { left: "", right: "" },
-          },
-          {
-              whichNerve: "ULNAR NERVE",
-              result: { left: "", right: "" },
-          },
-          {
-              whichNerve: "MEDIAN NERVE",
-              result: { left: "", right: "" },
-          }
+        {
+          whichNerve: "AXILLARY NERVE",
+          result: { left: "", right: "" },
+        },
+        {
+          whichNerve: "RADIAL NERVE",
+          result: { left: "", right: "" },
+        },
+        {
+          whichNerve: "ULNAR NERVE",
+          result: { left: "", right: "" },
+        },
+        {
+          whichNerve: "MEDIAN NERVE",
+          result: { left: "", right: "" },
+        }
       ],
       neurovascularExamRP: [
-          {
-              examName: "RADIAL PULSE",
-              result: { left: "", right: "" },
-          },
+        {
+          examName: "RADIAL PULSE",
+          result: { left: "", right: "" },
+        },
       ],
       neurovascularExamDCR: [
-          {
-              examName: "DISTAL CAPILLARY REFILL",
-              right: { less: "", greater: "" },
-              left: { less: "", greater: "" }
-          },
+        {
+          examName: "DISTAL CAPILLARY REFILL",
+          right: { less: "", greater: "" },
+          left: { less: "", greater: "" }
+        },
       ],
       wingingsOfScapula: [
-          {
-              testName: "WINGINGS OF SCAPULA",
-              result: { left: "", right: "" },
-          }
+        {
+          testName: "WINGINGS OF SCAPULA",
+          result: { left: "", right: "" },
+        }
       ],
       muscleWasting: [
-          {
-              testName: "MUSCLE WASTING",
-              result: { left: "", right: "" },
-          }
-      ],
-      cervicalSpineSpurlingTest: "None"
+        {
+          testName: "MUSCLE WASTING",
+          result: { left: "", right: "" },
+        }
+      ]
+    }
   }
-}
-clickBtn = e => {
-  this.setState({
-   renderView: +e.target.value
- }, console.log(this.state)); 
-};
-render(){
-  switch (this.state.renderView) {
-    case 1:
-      return <Submit list={this.state}/>;
-    default:
-      return <AssessmentForm clickBtn={this.clickBtn} values={this.state}/>;
+  clickBtn = e => {
+    this.setState({
+      renderView: +e.target.value
+    }, console.log(this.state));
+  };
+  render() {
+    switch (this.state.renderView) {
+      case 1:
+        return <Submit list={this.state} />;
+      default:
+        return <AssessmentForm clickBtn={this.clickBtn} values={this.state} />;
+    }
   }
-}
 }
 export default App;
 
