61e86fd627a9bbbd4cac8d644e2ad6e5442ff30b
sandeepkamishetti <59001664+sandeepkamishetti@users.noreply.github.com>
2020-06-17 20:54:21 +0530

    Added dummy state variable.
    Resolving the useless constructor warning

--- src/Submit.js
+++ src/Submit.js
@@ -4,6 +4,9 @@ import './Submit.css'
 class Submit extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            
+        }
     }
     render() {
         var Label1 = "(< 2 Sec)";
